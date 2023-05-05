9865e38185a9a6126fe5255923594c37b38eb581
Harrsh Patel <dev@harrsh.com>
2023-05-05 23:52:12 +0530

    Added product to redux

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,72 @@
 import { GetServerSideProps } from 'next';
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { productListResponse } from './api/product/list';
 import axiosInstance from '@/libs/interceptor';
 import env from '@/utils/env';
+import { useAppDispatch, useAppSelector } from '@/hooks/redux';
+import { setProducts } from '@/redux/slice/products.slice';
+import { useSession } from 'next-auth/react';
+import toast from '@/libs/toast';
+import NoProduct from '@/components/Home/NoProduct';
+import Head from 'next/head';
+import { Box } from '@mui/material';
 
 const Home = ({ data }: { data: productListApiResponse }) => {
-    console.log(data);
+    const { data: session } = useSession();
+    const products = useAppSelector((state) => state.products.products);
+    const dispatch = useAppDispatch();
 
-    return <div>Home</div>;
+    const { message, products: resProducts, success } = data;
+
+    useEffect(() => {
+        const data = [];
+
+        for (let product of resProducts) {
+            const productData = {
+                ...product,
+            };
+
+            const isFavorite =
+                session && productData.favoriteBy
+                    ? productData.favoriteBy.some(
+                          (el) => el.id === +session.user.id
+                      )
+                    : false;
+
+            delete productData.favoriteBy;
+            data.push({
+                ...productData,
+                isFavorite,
+            });
+        }
+
+        dispatch(
+            setProducts({
+                products: data,
+            })
+        );
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [resProducts]);
+
+    if (!success) {
+        toast(message);
+        return <NoProduct />;
+    }
+
+    if (!products.length) {
+        return <NoProduct />;
+    }
+
+    return (
+        <Fragment>
+            <Head>
+                <title>Home</title>
+            </Head>
+
+            <Box>Home</Box>
+        </Fragment>
+    );
 };
 
 export default Home;
