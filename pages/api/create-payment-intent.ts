8a4ccea734396d29d9b053a3a819f97be0f95c6c
Harrsh Patel <dev@harrsh.com>
2023-05-07 12:50:57 +0530

    Tweaks

--- pages/api/create-payment-intent.ts
+++ pages/api/create-payment-intent.ts
@@ -1,8 +1,11 @@
 import response from '@/libs/response';
 import env from '@/utils/env';
 import type { NextApiRequest, NextApiResponse } from 'next';
-const stripe = require('stripe')(env.stripe.secretKey);
-
+// const stripe = require('stripe')(env.stripe.secretKey);
+import stripeInstance from 'stripe';
+const stripe = new stripeInstance(env.stripe.secretKey, {
+    apiVersion: '2022-11-15',
+});
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         if (req.method !== 'POST') {
